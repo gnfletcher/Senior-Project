e44469f379550c418bf5a83ce1b6a8e1c511fb9d
Greg Fletcher <gnfletcher@gmail.com>
2018-03-08 10:44:07 -0500

    added duty schedule and switch pages.

--- template/switchrequest.php
+++ template/switchrequest.php
@@ -15,7 +15,7 @@ include 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>RLUH - Programs</title>
+    <title>RLUH - Duty</title>
     <!-- Bootstrap core CSS-->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom fonts for this template-->
@@ -31,7 +31,7 @@ include 'connect.php';
 <body class="fixed-nav  bg-dark" id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Program Scheduling</a>
+    <a class="navbar-brand" href="index.html">Duty Schedule Switch Request</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
             data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
             aria-label="Toggle navigation">
@@ -207,8 +207,8 @@ include 'connect.php';
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <h2> RA Program Scheduling </h2>
-                <p> Welcome to RA Program Scheduling Page! </p>
+                <h2> RA Duty Schedule Switch</h2>
+                <p> Welcome to RA Duty Schedule Switch Request Page! </p>
             </div>
         </div>
     </div>
@@ -222,69 +222,7 @@ include 'connect.php';
                 style="border: 0; float:right;" width="700" height="400" frameborder="0" scrolling="no"></iframe>
     </div>
 
-    <div class = "main-container">
-        <div class = "content-container">
-            <div class = "row">
-                <div class = "yellow-bar">
-                    <h3 class = "header-text"> Community Builder Programs </h3>
-                </div>
-            </div>
-            <table style = "width: 100%" class = "info-text">
-                <tr>
-                    <th style = "font-size: 1.1em"> Program Date </th>
-                    <th style = "font-size: 1.1em"> Title</th>
-                    <th style = "font-size: 1.1em"> Points </th>
-                </tr>
-                <tr>
-                    <td> 2018-01-24 </td>
-                    <td> Paint the Hood </td>
-                    <td> 3 pts. </td>
-                </tr>
-                <tr>
-                    <td> 2018-02-14 </td>
-                    <td> Love is in the air </td>
-                    <td> 3 pts. </td>
-                </tr>
-            </table>
-        </div>
 
-        <div class = "margin"> </div>
-
-        <div class = "content-container">
-            <div class = "row">
-                <div class = "yellow-bar">
-                    <h3 class = "header-text"> Campus Outreach Programs </h3>
-                </div>
-            </div>
-            <table style = "width: 100%" class = "info-text">
-                <tr>
-                    <th style = "font-size: 1.1em"> Program Date </th>
-                    <th style = "font-size: 1.1em"> Title</th>
-                    <th style = "font-size: 1.1em"> Points </th>
-                </tr>
-                <tr>
-                    <td> 2018-04-01 </td>
-                    <td> Prank Your Residents! </td>
-                    <td> 100 pts. </td>
-                </tr>
-            </table>
-        </div>
-    </div>
-
-    <div class = "right-main-container">
-        <div class = "content-container">
-            <!-- Program Information -->
-            <div class = "row">
-                <div class = "yellow-bar">
-                    <h3 class = "header-text"> Announcements </h3>
-                </div>
-            </div>
-            <div class = "container">
-                <p class = "info-text"> Spring Break! </p>
-                <p class = "info-text"> Apply to Graduate by 2/28! </p>
-                <p class = "info-text"> Switch </p>
-            </div>
-        </div>
 </div>
 
 <!-- Logout Modal-->
