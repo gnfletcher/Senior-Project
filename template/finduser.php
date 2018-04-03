b3be86bb31d3c6659e864a7d6d655722fd15ec5f
Greg Fletcher <gnfletcher@gmail.com>
2018-04-02 21:17:37 -0400

    Redirect

--- template/finduser.php
+++ template/finduser.php
@@ -2,10 +2,10 @@
 		$email = $_GET["email"];
 		$sql = "SELECT user_id FROM users u WHERE u.email = '$email'";
 		$result = $conn->query($sql);
-		$user_ID = "";
+		$user_id = "2";
 		if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-            $user_ID = $row["user_ID"];    
+            /*$user_id = $row["user_id"]; */   
             }
         } else {
             echo '';;
