377c52c061a07914605d2c47e9d9c62e05871bc8
Greg Fletcher <gnfletcher@gmail.com>
2018-04-03 13:07:30 -0400

    Function to direct to proper homepage

--- template/finduser.php
+++ template/finduser.php
@@ -6,10 +6,10 @@ include 'connect.php';
 		$user_id = "";
 		if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-				$user_id = $row["user_id"];
-				header("Location: https://swang.devspace.link/dev/rahome.php?user_id=" . $user_id);
-				die();			
+				$user_id = $row["user_id"];			
             }
+			header("Location: https://swang.devspace.link/dev/rahome.php?user_id=" . $user_id);
+			die();
         } else {
             header("Location: https://swang.devspace.link/dev/register.html");
 			die();
