f0b0ac90629975abe861eecd92d8996146d5af1d
Greg Fletcher <gnfletcher@gmail.com>
2018-04-03 13:06:05 -0400

    Function to direct to proper homepage

--- template/finduser.php
+++ template/finduser.php
@@ -6,32 +6,11 @@ include 'connect.php';
 		$user_id = "";
 		if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-            $user_id = $row["user_id"];		
+				$user_id = $row["user_id"];
+				header("Location: https://swang.devspace.link/dev/rahome.php?user_id=" . $user_id);
+				die();			
             }
         } else {
             header("Location: https://swang.devspace.link/dev/register.html");
 			die();
         }
-		$sql2 = "(SELECT *, 'ra' as type FROM resident_assistants r WHERE r.user_id = '$user_id')
-		UNION
-		(SELECT *, 'ard' as type FROM assistant_rds a WHERE a.user_id = '$user_id')
-		UNION
-		(SELECT *, 'rd' as type FROM resident_directors d WHERE d.user_id = '$user_id')";
-		$result = $conn->query($sql2);
-		if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                if('ra' = $row["type"]){
-					header("Location: https://swang.devspace.link/dev/rahome.php?user_id=" . $user_id);
-					die();
-				} elseif('ard' = $row["type"]){
-					header("Location: https://swang.devspace.link/dev/ardhome.php?user_id=" . $user_id);
-					die();
-				} elseif('rd' = $row["type"]){
-					header("Location: https://swang.devspace.link/dev/rdhome.php?user_id=" . $user_id);
-					die();
-				}
-            }
-        } else {
-            header("Location: https://swang.devspace.link/dev/userNotFound.html");
-			die();
-		}
