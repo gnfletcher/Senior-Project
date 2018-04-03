72bd1339469efd7b1124f1a563f5493a8394bec7
Greg Fletcher <gnfletcher@gmail.com>
2018-04-02 21:29:40 -0400

    Redirect

--- template/finduser.php
+++ template/finduser.php
@@ -3,15 +3,15 @@ include 'connect.php';
 		$email = $_GET["email"];
 		$sql = "SELECT user_id FROM users u WHERE u.email = '$email'";
 		$result = $conn->query($sql);
-		$user_ID = "";
+		$user_id = "";
 		if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-            $user_ID = $row["user_ID"];   
+            $user_id = $row["user_id"];   
             }
         } else {
             echo '';;
         }
-		header("Location: https://swang.devspace.link/dev/rahome.php?user_ID=" . $user_ID);
+		header("Location: https://swang.devspace.link/dev/rahome.php?user_id=" . $user_id);
 		die();
 		
         ?>
