fcd3f2e4bf9b31edc61e2e43ff2f70c5bb1c3d0f
jillpulli <jillianpulicicchio@gmail.com>
2018-04-09 23:31:45 -0400

    redirect conflogsubmit to success message and return to rahome

--- template/conflogsubmit.php
+++ template/conflogsubmit.php
@@ -43,3 +43,33 @@ if ((isset($_POST)) || (isset($POST['submit']))) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>RLUH Website</title>
+        <!-- Bootstrap core CSS-->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom fonts for this template-->
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- Page level plugin CSS-->
+        <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin.css" rel="stylesheet">
+    </head>
+    <body>
+        <h3>Successful Request!</h3>
+        <p> You will be notified as soon as possible on your request</p>
+            <script language="javascript" type="text/javascript">
+                <!--
+
+                window.setTimeout('window.location = "rahome.php"',2500);
+                // -->
+            </script>
+    </body>
+
+</html>
