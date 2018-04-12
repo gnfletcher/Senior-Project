d0c255eee4ada1c409210fa64fcd50f9da3a8348
jillpulli <jillianpulicicchio@gmail.com>
2018-04-11 23:04:04 -0400

    update duty scheduling nav bar

--- template/dutyschedule.php
+++ template/dutyschedule.php
@@ -31,76 +31,87 @@ $user_type = $_SESSION["user_type"];
 <body class="sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Duty Scheduling</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-            aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="rahome.php">
-            <i class="fa fa-fw fa-dashboard"></i>
-            <span class="nav-link-text">Home</span>
-          </a>
-        </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-area-chart"></i>
-            <span class="nav-link-text">Programs</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseComponents">
-            <li>
-              <a href="programproposal.php">Program Proposal</a>
-            </li>
-            <li>
-              <a href="programscheduling.php">Program Scheduling</a>
-            </li>
+      <a class="navbar-brand" href="rahome.php?user_id=<?php echo $user_id; ?>">RLUH RA MAIN</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+              data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+              aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                  <a class="nav-link" href="rahome.php?user_id=<?php echo $user_id; ?>">
+                      <i class="fa fa-fw fa-dashboard"></i>
+                      <span class="nav-link-text">Home</span>
+                  </a>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents"
+                     data-parent="#exampleAccordion">
+                      <i class="fa fa-fw fa-area-chart"></i>
+                      <span class="nav-link-text">Programs</span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseComponents">
+                      <li>
+                          <a href="programproposal.php?user_id=<?php echo $user_id; ?>">Program Proposal</a>
+                      </li>
+                      <li>
+                          <a href="programscheduling.php?user_id=<?php echo $user_id; ?>">Program Scheduling</a>
+                      </li>
+                  </ul>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseDutyComponents"
+                     data-parent="#exampleAccordion">
+                      <i class="fa fa-fw fa-table"></i>
+                      <span class="nav-link-text">Duty</span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseDutyComponents">
+                      <li>
+                          <a href="dutyschedule.php?user_id=<?php echo $user_id; ?>">Duty Scheduling</a>
+                      </li>
+                      <li>
+                          <a href="switchrequest.php?user_id=<?php echo $user_id; ?>">Switch Duty Request</a>
+                      </li>
+                  </ul>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+                  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseConfComponents"
+                     data-parent="#exampleAccordion">
+                      <i class="fa fa-fw fa-table"></i>
+                      <span class="nav-link-text"> Confiscation Logs </span>
+                  </a>
+                  <ul class="sidenav-second-level collapse" id="collapseConfComponents">
+                      <li>
+                          <a href="confiscationform.php?user_id=<?php echo $user_id; ?>"> Submit an Incident </a>
+                      </li>
+                      <li>
+                          <a href="confiscationlog.php?user_id=<?php echo $user_id; ?>"> View Past Incidents </a>
+                      </li>
+                  </ul>
+              </li>
+              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+                  <a class="nav-link" href="usersettings.html">
+                      <i class="fa fa-fw fa-wrench"></i>
+                      <span class="nav-link-text">Settings</span>
+                  </a>
+              </li>
           </ul>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseDutyComponents" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-table"></i>
-            <span class="nav-link-text">Duty</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseDutyComponents">
-            <li>
-              <a href="dutyschedule.php">Duty Scheduling</a>
-            </li>
-            <li>
-              <a href="switchrequest.php">Switch Duty Request</a>
-            </li>
+          <ul class="navbar-nav sidenav-toggler">
+              <li class="nav-item">
+                  <a class="nav-link text-center" id="sidenavToggler">
+                      <i class="fa fa-fw fa-angle-left"></i>
+                  </a>
+              </li>
           </ul>
-        </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-            <a class="nav-link" href="confiscationform.php">
-                <i class="fa fa-fw fa-file"></i>
-                <span class="nav-link-text">Confiscation Log</span>
-            </a>
-        </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-            <a class="nav-link" href="usersettings.html">
-                <i class="fa fa-fw fa-wrench"></i>
-                <span class="nav-link-text">Settings</span>
-            </a>
-        </li>
-        </ul>
-        <ul class="navbar-nav sidenav-toggler">
-            <li class="nav-item">
-                <a class="nav-link text-center" id="sidenavToggler">
-                    <i class="fa fa-fw fa-angle-left"></i>
-                </a>
-            </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-                    <i class="fa fa-fw fa-sign-out"></i>Logout</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+                      <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+              </li>
+          </ul>
+      </div>
+  </nav>
 
 <!-- Main Content -->
 <div class="content-wrapper">
